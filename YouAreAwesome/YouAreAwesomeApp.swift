1118e1df35f14cfc4de78a6e46da84fcd108857d
s3670860 <s3670860@student.rmit.edu.au>
2024-12-04 16:38:50 +1000

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by nathan albinger on 4/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
