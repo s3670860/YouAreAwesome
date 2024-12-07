1b9302bd4008dbc37dc9d154a50e1f08db27775a
s3670860 <s3670860@student.rmit.edu.au>
2024-12-07 19:12:26 +1000

    Version 7.0 tutorial finished - gradients

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,70 +10,98 @@ import SwiftUI
 struct ContentView: View {
     @State private var stringyMcTextFace = ""
     var body: some View {
+//                add parameter to the stack to make adjustment
+//        VStack(alignment: .center, spacing: 55) {
+//            Image(systemName: "6.circle")
+////                .resizable()
+////                .scaledToFit()
+//        }
         
-        VStack(alignment: .center, spacing: 1) {
-            Image(systemName: "5.circle")
-                .resizable()
-                .scaledToFit()
-        }
-        VStack {
-//        add parameter to the stack to make adjustments
-//        VStack (spacing: 0) {
-            Image(systemName: "4.circle")
-                .resizable()
-                .scaledToFit()
-//                            .imageScale(.large)
-                .foregroundStyle(.purple)
-//                .padding(.trailing, 30.0)
-//                .padding(.bottom, 100)
-//                .background(.blue)
-            
+        ZStack {
+            //            Color.lawn.opacity(0.5)
             Rectangle()
-                .foregroundColor(.purple)
-//                .scaledToFit()
-            Text(stringyMcTextFace)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-//             center the text if on more than one line
-                .multilineTextAlignment(.center)
-                .foregroundColor(.red)
-                .italic()
-//             create a frame so the buttons don't move when text appears
-                .frame(height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-//            Add a second frame set to infinite width so text adapts to different screen size
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-//             create a border to assist with debugging
-                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                .padding()
-            
-            HStack {
-                Button("Awesome") {
-//                     This is the action performed when the button is pressed
-                    stringyMcTextFace = "You Are Awesome!"
+//            either fill with a color and default gradient
+//                .fill(.lawn.gradient)
+//            or create your own gradient
+                .fill(
+                    Gradient(colors: [Color("Lawn"), .indigo])
+                    )
+                .ignoresSafeArea()
+            VStack {
+                
+    //        add parameter to the stack to make adjustments
+                
+    //        VStack (spacing: 0) {
+                
+//                removed tutorial elements
+                
+    //            Image(systemName: "4.circle")
+    //                .resizable()
+    //                .scaledToFit()
+    //                            .imageScale(.large)
+    //                .foregroundStyle(.purple)
+    //                .padding(.trailing, 30.0)
+    //                .padding(.bottom, 100)
+    //                .background(.blue)
+                
+    //            Rectangle()
+    //                .foregroundColor(.purple)
+    //                .scaledToFit()
+                Text("You Have Skills")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                    .padding()
+    //            using a color set created in a palette
+                    .background(Color("Vermillion"))
+                    .cornerRadius(15)
+                Spacer()
+                Text(stringyMcTextFace)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+    //             center the text if on more than one line
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .italic()
+    //             create a frame so the buttons don't move when text appears
+                    .frame(height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    //            Add a second frame set to infinite width so text adapts to different screen size
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+    //             create a border to assist with debugging
+//                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .padding()
+                Spacer()
+                HStack {
+                    Button("Awesome") {
+    //                     This is the action performed when the button is pressed
+                        stringyMcTextFace = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Button("Great") {
+    //                     This is the action performed when the button is pressed
+                        stringyMcTextFace = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Button ("Greater"){
+                        stringyMcTextFace = "You Are The Greatest!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Button("Best") {
+                        stringyMcTextFace = "You Are The Best!"
+                    }
+                    .buttonStyle(.bordered)
                 }
-                .buttonStyle(.borderedProminent)
-                Button("Great") {
-//                     This is the action performed when the button is pressed
-                    stringyMcTextFace = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
-                Button ("Greater"){
-                    stringyMcTextFace = "You Are The Greatest!"
-                }
-                .buttonStyle(.borderedProminent)
-                Button("Best") {
-                    stringyMcTextFace = "You Are The Best!"
-                }
-                .buttonStyle(.bordered)
-            }
-            Text("Version 5.0")
-                .bold()
-//                .padding()
-                .foregroundColor(.red)
+                .tint(Color("Sunset"))
+                
+                Text("Version 7.0")
+                    .bold()
+    //                .padding()
+                    .foregroundColor(.red)
 
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
