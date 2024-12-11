491f86acce2690a406d5627c55062bc4346cc8f0
s3670860 <s3670860@student.rmit.edu.au>
2024-12-11 21:10:29 +1000

    double check

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct ContentView: View {
     
-//    create text vars that can be manipulated with screen redraws
+    //    create text vars that can be manipulated with screen redraws
     @State private var stringyMcTextFace = ""
     @State private var convertedTemperture = ""
     @State private var inputTemperature = ""
@@ -18,118 +18,127 @@ struct ContentView: View {
     
     var body: some View {
         
-//                add parameter to the stack to make adjustment
+        //                add parameter to the stack to make adjustment
         
-//        VStack(alignment: .center, spacing: 55) {
-//            Image(systemName: "6.circle")
-////                .resizable()
-////                .scaledToFit()
-//        }
+        //        VStack(alignment: .center, spacing: 55) {
+        //            Image(systemName: "6.circle")
+        ////                .resizable()
+        ////                .scaledToFit()
+        //        }
         
         ZStack {
             //            Color.lawn.opacity(0.5)
-            Rectangle()
+                        Rectangle()
             
-            //            either fill with a color and default gradient
+                        //            either fill with a color and default gradient
             
-            //                .fill(.lawn.gradient)
+                        //                .fill(.lawn.gradient)
             
-            //            or create your own gradient
+                        //            or create your own gradient
             
-                .fill(
-                    Gradient(colors: [Color("Lawn"), .indigo])
-                )
-                .ignoresSafeArea()
+                            .fill(
+                                Gradient(colors: [Color("Midnight"), Color("RealMidnight")])
+                            )
+                            .ignoresSafeArea()
             
-            VStack(spacing: 20) {
-                
-//                        add parameter to the stack to make adjustments
-//                
-//                        VStack (spacing: 0) {
-//                
-//                                removed tutorial elements
-//                
-//                            Image(systemName: "4.circle")
-//                                .resizable()
-//                                .scaledToFit()
-//                                            .imageScale(.large)
-//                                .foregroundStyle(.purple)
-//                                .padding(.trailing, 30.0)
-//                                .padding(.bottom, 100)
-//                                .background(.blue)
-//                
-//                            Rectangle()
-//                                .foregroundColor(.purple)
-//                                .scaledToFit()
-                Text("Baba's Temperature Converter")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-//                             create a frame so the buttons don't move when text appears
-                    .frame(height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-//                            Add a second frame set to infinite width so text adapts to different screen size
-                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .padding(.top)
-//                            using a color set created in a palette
-                    .background(Color("Vermillion"))
-                    .cornerRadius(15)
-                Spacer()
-                //                Input Field
-                TextField("Enter Temperture", text: $inputTemperature)
-                    .keyboardType(.decimalPad)
-                    .background(Color.yellow)
-                    .padding()
-                    .cornerRadius(15)
-                    .shadow(radius: 1)
-                
-                Toggle("Celcius to Fahrenheight", isOn: $isCelciusToFahrenheight)
-                    .padding()
-                
-                Button(action: {
-                    calculateTemperature()
+            
+            
+            //            embed the Vstack in a GeometryReader so the reletive geometry can be assigned to a variable
+            GeometryReader {geometry in
+                VStack(spacing: 20) {
                     
-                }) {
-                    Text("Convert")
-                        .font(.headline)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.black)
-                        .cornerRadius(8)
-                }
-                if !convertedTemperture.isEmpty {
-                    Text("Converted Temperature: \(convertedTemperture)")
-                        .font(.title2)
-                        .frame(height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            //  Add a second frame set to infinite width so text adapts to different screen size
+                    //                        add parameter to the stack to make adjustments
+                    //
+                    //                        VStack (spacing: 0) {
+                    //
+                    //                                removed tutorial elements
+                    //
+                    //                            Image(systemName: "4.circle")
+                    //                                .resizable()
+                    //                                .scaledToFit()
+                    //                                            .imageScale(.large)
+                    //                                .foregroundStyle(.purple)
+                    //                                .padding(.trailing, 30.0)
+                    //                                .padding(.bottom, 100)
+                    //                                .background(.blue)
+                    //
+                    //                            Rectangle()
+                    //                                .foregroundColor(.purple)
+                    //                                .scaledToFit()
+                    Text("Baba's Temperature Converter")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    //                             create a frame so the buttons don't move when text appears
+                        .frame(height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    //                            Add a second frame set to infinite width so text adapts to different screen size
                         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                        .padding()
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                }
-                    Spacer()
+                        .padding(.top)
+                    //                            using a color set created in a palette
+                        .background(Color("Vermillion"))
+                        .cornerRadius(15)
                     
-//                                    Text(tempCalculation)
-//                                        .font(.largeTitle)
-//                                        .fontWeight(.heavy)
-//                                        .minimumScaleFactor(0.5)
-//                        //             center the text if on more than one line
-//                                        .multilineTextAlignment(.center)
-//                                        .foregroundColor(.red)
-//                        //             create a frame so the buttons don't move when text appears
-//                                        .frame(height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-//                        //            Add a second frame set to infinite width so text adapts to different screen size
-//                                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-//                        //             create a border to assist with debugging
-//                                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-//                                        .padding()
-//                    
-//                                    let myInt = Double(userInput) ?? 0
-//                                    let calcValue = (myInt - 32) * (5/9)
-//                    Button("Calculate") {
-//                        //                    tempCalculation  = String(calcValue)
-//                    }
-//                    .buttonStyle(.borderedProminent)
-//                    .tint(Color("Sunset"))
+//                    Swift only allows 10 child elements in a view so workaround is add elements into a Group
+                    Group {
+                        Spacer()
+                        //                Input Field
+                        TextField("Enter Temperture", text: $inputTemperature)
+                            .keyboardType(.decimalPad)
+                            .background(Color.yellow)
+                            .padding()
+                            .cornerRadius(15)
+                            .shadow(radius: 1)
+                        
+                        Toggle("Celcius to Fahrenheight", isOn: $isCelciusToFahrenheight)
+                            .padding()
+                        
+                        Button(action: {
+                            calculateTemperature()
+                            
+                        }) {
+                            Text("Convert")
+                                .font(.headline)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.black)
+                                .cornerRadius(8)
+                        }
+                        if !convertedTemperture.isEmpty {
+                            Text("Converted Temperature: \(convertedTemperture)")
+                                .font(.title2)
+                                .frame(height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            //  Add a second frame set to infinite width so text adapts to different screen size
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                                .padding()
+                                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                        }
+                        Spacer()
+                    }
+                    
+                    
+                    //                                    Text(tempCalculation)
+                    //                                        .font(.largeTitle)
+                    //                                        .fontWeight(.heavy)
+                    //                                        .minimumScaleFactor(0.5)
+                    //                        //             center the text if on more than one line
+                    //                                        .multilineTextAlignment(.center)
+                    //                                        .foregroundColor(.red)
+                    //                        //             create a frame so the buttons don't move when text appears
+                    //                                        .frame(height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    //                        //            Add a second frame set to infinite width so text adapts to different screen size
+                    //                                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    //                        //             create a border to assist with debugging
+                    //                                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    //                                        .padding()
+                    //
+                    //                                    let myInt = Double(userInput) ?? 0
+                    //                                    let calcValue = (myInt - 32) * (5/9)
+                    //                    Button("Calculate") {
+                    //                        //                    tempCalculation  = String(calcValue)
+                    //                    }
+                    //                    .buttonStyle(.borderedProminent)
+                    //                    .tint(Color("Sunset"))
                     Spacer()
                     
                     Text(stringyMcTextFace)
@@ -147,7 +156,15 @@ struct ContentView: View {
                     //             create a border to assist with debugging
                     //                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
                         .padding()
-               
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: geometry.size.width * (2/3), height: 1)
+                    
+                    //                       Divider()
+                    //                        .background(.black)
+                    //                        .padding()
+                    //                        .frame(width: 150.0)
+                    
                     HStack {
                         Button("Awesome") {
                             //                     This is the action performed when the button is pressed
@@ -159,6 +176,8 @@ struct ContentView: View {
                             stringyMcTextFace = "You Are Great!"
                         }
                         .buttonStyle(.borderedProminent)
+                        Divider()
+                            .padding()
                         Button ("Greater"){
                             stringyMcTextFace = "You Are The Greatest!"
                         }
@@ -177,7 +196,8 @@ struct ContentView: View {
                     
                 }
                 .padding()
-                
+            }
+            
             
             
             
